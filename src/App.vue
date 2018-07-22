7c94af25017e809aef5bd31cbfb09ac127bd2de3
kiseong.park <kiseong.park@stunitas.com>
2018-07-22 17:40:40 +0900

    [vue] github page test

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hello, neon.D!'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+body {
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    background-image: url('./assets/bg_normal.jpg');
+    background-size: cover;
+    background-position: center;
+}
+</style>
